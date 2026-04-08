7ea8c12c807c6333cb69c1985f85de7ad2ca3921
Thineth <thinethgeewinda660@gmail.com>
2026-04-08 11:18:07 +0530

    feat(src): update the maze to have the different weight tiles

--- src/maze_logic.py
+++ src/maze_logic.py
@@ -1,27 +1,22 @@
 from models.graph import Graph
-# Maze representation
 
-# S = Start
-# E = End
-# . = Walkable
-# # = Wall
-
+# M = Mud, W = Water
 maze = [
-    ['S', '.', '.', '#'],
-    ['.', '#', '.', '.'],
-    ['.', '.', '.', 'E']
+    ['S', '.', 'W', '.'],
+    ['.', 'W', '.', '.'],
+    ['.', 'M', '.', 'E']
 ]
 
 rows = len(maze)
 cols = len(maze[0])
+CELL_SIZE = 80
 
 def is_valid(r, c):
     return 0 <= r < rows and 0 <= c < cols and maze[r][c] != '#'
 
-def find_start_end():
-    start = None
-    end = None
 
+def find_start_end():
+    start, end = None, None
     for r in range(rows):
         for c in range(cols):
             if maze[r][c] == 'S':
@@ -30,33 +25,32 @@ def find_start_end():
                 end = (r, c)
     return start, end
 
+
 def build_graph():
     graph = Graph()
-
     for r in range(rows):
         for c in range(cols):
             if maze[r][c] != '#':
                 current = (r, c)
                 graph.add_node(current)
-
                 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-
                     if is_valid(nr, nc):
                         neighbor = (nr, nc)
 
-                        if maze[nr][nc] == '.':
-                            weight = 1
-                        elif maze[nr][nc] == 'E':
-                            weight = 1
+                        target_tile = maze[nr][nc]
+                        if target_tile == 'W':
+                            weight = 10  # High cost
+                        elif target_tile == 'M':
+                            weight = 5  # Medium cost
                         else:
-                            weight = 1
+                            weight = 1  # Low cost
 
                         graph.add_edge(current, neighbor, weight)
-
     return graph
+
 
 def display_maze(maze, path=None):
     for r in range(len(maze)):
